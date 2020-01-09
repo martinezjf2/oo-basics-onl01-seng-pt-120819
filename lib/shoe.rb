bdcb58a81f8813bd067b3d29fb7d691ff8402151
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-09 21:48:38 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,12 @@
 require "pry"
 
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material
+  attr_reader :brand, :condition
 
-  def initialize(brand)
+  def initialize(brand, condition)
     @brand = brand
+    @condition = condition
   end
 
   def cobble
@@ -19,4 +20,4 @@ end
 
 
 new = Shoe.new
-# Shoe.condition = "new"
+Shoe.condition = "new"
